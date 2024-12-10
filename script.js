6993e6dcc80a7ef614508d0f3f35ed211b848a40
upiidesu <luthfifauzan857@gmail.com>
2024-12-10 08:05:40 +0700

    membuat view site

--- script.js
+++ script.js
@@ -20,30 +20,30 @@ document.addEventListener("scroll", function () {
 });
 
 /* ==================== SCROLL SECTION ACTIVE LINK ==================== */
-// const sections = document.querySelectorAll("section[id]");
+const sections = document.querySelectorAll("section[id]");
 
-// const scrollActive = () => {
-//     const scrollDown = window.scrollY;
+const scrollActive = () => {
+    const scrollDown = window.scrollY;
 
-//     sections.forEach((current) => {
-//         const sectionHeight = current.offsetHeight,
-//             sectionTop = current.offsetTop - 58,
-//             sectionId = current.getAttribute("id"),
-//             sectionsClass = document.querySelector(
-//                 ".nav-list a[href*=" + sectionId + "]"
-//             );
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute("id"),
+            sectionsClass = document.querySelector(
+                ".navbar-nav a[href*=" + sectionId + "]"
+            );
 
-//         if (
-//             scrollDown > sectionTop &&
-//             scrollDown <= sectionTop + sectionHeight
-//         ) {
-//             sectionsClass.classList.add("active-link");
-//         } else {
-//             sectionsClass.classList.remove("active-link");
-//         }
-//     });
-// };
-// window.addEventListener("scroll", scrollActive);
+        if (
+            scrollDown > sectionTop &&
+            scrollDown <= sectionTop + sectionHeight
+        ) {
+            sectionsClass.classList.add("active-link");
+        } else {
+            sectionsClass.classList.remove("active-link");
+        }
+    });
+};
+window.addEventListener("scroll", scrollActive);
 
 /* ==================== HAMBURGER MENU ==================== */
 //Toggle class active hamburger menu
